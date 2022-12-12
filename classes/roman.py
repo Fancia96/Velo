21442d9dffea993e2c1ec2624c730296729f4851
Fancia666 <s18425@pjwstk.edu.pl>
2022-12-12 23:27:26 +0100

    roman numerals test adding code

--- classes/roman.py
+++ classes/roman.py
@@ -1,9 +1,11 @@
 
 
 def roman(number):
-    num = [1, 4, 5, 9, 10]
-    sym = ["I", "IV", "V", "IX", "X"]
-    i = 4
+    num = [1, 4, 5, 9, 10, 40, 50, 90,
+           100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL",
+           "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
 
     return_number = ""
 
