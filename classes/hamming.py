f3b76e1fe01179016e3e8d19997544feb70b80e6
Fancia666 <s18425@pjwstk.edu.pl>
2022-12-12 14:55:44 +0100

    hamming test rest of tests and refactoring

--- classes/hamming.py
+++ classes/hamming.py
@@ -1,6 +1,8 @@
 
 def distance(s1, s2):
 
-    value = sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
-    print()
-    return value
+    if len(s1) != len(s2):
+        raise ValueError("test message")
+    else:
+        value = sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+        return value
--- tests/hammingTest.py
+++ tests/hammingTest.py
@@ -8,38 +8,38 @@ class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(hamming.distance("", ""), 0)
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_single_letter_identical_strands(self):
         self.assertEqual(hamming.distance("A", "A"), 0)
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_single_letter_different_strands(self):
         self.assertEqual(hamming.distance("G", "T"), 1)
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_long_identical_strands(self):
         self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_long_different_strands(self):
         self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_disallow_first_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("AATG", "AAA")
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_disallow_second_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("ATA", "AGTG")
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_disallow_left_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("", "G")
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_disallow_right_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("G", "")
