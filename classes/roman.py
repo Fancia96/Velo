fdc1b9f16870fc48b797a8158cb767f6115536d2
Fancia666 <s18425@pjwstk.edu.pl>
2022-12-12 15:52:30 +0100

    roman numerals test adding code for test_6_being_5_1_is_vi

--- classes/roman.py
+++ classes/roman.py
@@ -1,8 +1,8 @@
 
 
 def roman(number):
-    num = [1, 4, 5]
-    sym = ["I", "IV", "V"]
+    num = [1, 4, 5, 6]
+    sym = ["I", "IV", "V", "VI"]
     i = 2
 
     return_number = ""
--- tests/romanNumeralsTest.py
+++ tests/romanNumeralsTest.py
@@ -25,7 +25,7 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_5_is_a_single_v(self):
         self.assertEqual(roman(5), "V")
 
-    @unittest.skip("reason for skipping")
+    #@unittest.skip("reason for skipping")
     def test_6_being_5_1_is_vi(self):
         self.assertEqual(roman(6), "VI")
 
