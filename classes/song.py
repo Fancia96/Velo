03f76972476c4bcb1fb28d160617bcbd92046cb8
Fancia666 <s18425@pjwstk.edu.pl>
2022-12-13 12:12:10 +0100

    few verses added tests plus code

--- classes/song.py
+++ classes/song.py
@@ -1,5 +1,5 @@
 
-def return_verse(verse):
+def return_verse(verse=None, end_verse=None):
     song_array = []
     f = open('../data/song.txt', 'r')
     for line in f:
@@ -7,9 +7,18 @@ def return_verse(verse):
             song_array.append(line.strip())
     f.close()
 
-    for i in range (0, len(song_array)):
-        if i+1 == verse:
+    verses = ""
+
+    for i in range(0, len(song_array)):
+        if i+1 == verse and end_verse == None:
             return song_array[i]
 
+        if end_verse != None and i+1 >= verse and i+1 <= end_verse:
+            verses += song_array[i]+"\n"
+
+    return verses
+
 if __name__ == '__main__':
-    print(return_verse(1))
+    pass
+    # print(return_verse(1))
+    #print(return_verse(4, 6))
--- tests/songTest.py
+++ tests/songTest.py
@@ -6,6 +6,8 @@ from classes.song import return_verse
 
 class SongTest(unittest.TestCase):
 
+    maxDiff = None
+
     def test_first_verse(self):
         vers_1 = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
         self.assertEqual(return_verse(1), vers_1)
@@ -17,3 +19,13 @@ class SongTest(unittest.TestCase):
     def test_eleventh_verse(self):
         vers_11 = "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
         self.assertEqual(return_verse(11), vers_11)
+
+    def test_fourth_to_sixth_verse(self):
+        vers_4_to_6 = "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+
+        self.assertEqual(return_verse(4, 6), vers_4_to_6)
+
+    def test_tenth_to_twelfe_verse(self):
+        vers_10_to_12 = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+
+        self.assertEqual(return_verse(10, 12), vers_10_to_12)
