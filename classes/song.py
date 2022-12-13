ddf054eac505a51d3c8f3f87ea712ba7de7c7573
Fancia666 <s18425@pjwstk.edu.pl>
2022-12-13 22:17:21 +0100

    tests for exceptions

--- classes/song.py
+++ classes/song.py
@@ -1,21 +1,24 @@
-
 def return_verse(verse=None, end_verse=None):
+    if verse!=None and not str(verse).isnumeric():
+        raise Exception("value of a row must be a number")
+
     song_array = []
     f = open('../data/song.txt', 'r')
     for line in f:
-        if(len(line.strip()) != 0):
+        if (len(line.strip()) != 0):
             song_array.append(line.strip())
     f.close()
 
     verses = ""
 
     for i in range(0, len(song_array)):
-        if i+1 == verse and end_verse == None:
+        if i + 1 == verse and end_verse == None:
             return song_array[i]
-        if end_verse != None and i+1 >= verse and i+1 <= end_verse:
-            verses += song_array[i]+"\n"
+        if end_verse != None and i + 1 >= verse and i + 1 <= end_verse:
+            verses += song_array[i] + "\n"
 
     return verses
+
 
 def return_whole_song():
     number_of_verses = 0
@@ -26,8 +29,9 @@ def return_whole_song():
 
     return return_verse(1, number_of_verses)
 
+
 if __name__ == '__main__':
     pass
-    # print(return_verse(1))
-    #print(return_verse(4, 6))
-    #print(return_whole_song())
+    print(return_verse(""))
+    # print(return_verse(4, 6))
+    # print(return_whole_song())
--- tests/songTest.py
+++ tests/songTest.py
@@ -34,3 +34,29 @@ class SongTest(unittest.TestCase):
         whole_song = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\nOn the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\nOn the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
 
         self.assertEqual(return_whole_song(), whole_song)
+
+    def test_first_verse_not_equal(self):
+        vers_1 = "2134243242"
+        self.assertNotEqual(return_verse(1), vers_1)
+
+    def test_fourth_to_sixth_verse_not_equal(self):
+        vers_4_to_6 = "23467876427"
+
+        self.assertNotEqual(return_verse(4, 6), vers_4_to_6)
+
+    def test_return_whole_song_not_equal(self):
+        whole_song = "2342342342"
+
+        self.assertNotEqual(return_whole_song(), whole_song)
+
+    def test_one_verse_given_wrong_value(self):
+        self.assertRaises(Exception, return_verse, "a")
+
+    def test_fourth_to_sixth_given_both_wrong_values(self):
+        self.assertRaises(Exception, return_verse, "a", "b")
+
+    def test_fourth_to_sixth_given_first_wrong_values(self):
+        self.assertRaises(Exception, return_verse, "a", 1)
+
+    def test_fourth_to_sixth_given_second_wrong_values(self):
+        self.assertRaises(Exception, return_verse, 1, "a")
